57fdd940fe4bc24d4e1cc37b5d57ae1119e66118
sole1982 <soleadhernandez@gmail.com>
2023-11-23 23:37:00 -0300

    Esqueleto del proyecto/ componente Card
    Componente Card

---
+++ eccomerce/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Card from './component/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
---
+++ eccomerce/src/component/Card.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import "../styles/Card.css"
+
+export const Card = () => {
+  return (
+    <>
+<div  className="text-3xl font-bold object-none object-center"  >
+ 
+    <div className="cards ">
+    <div class="card black">
+        <p class="tip">Deportivo</p>
+        <p class="second-text">Ropa deportiva</p>
+    </div>
+    <div class="card black">
+        <p class="tip">Casual</p>
+        <p class="second-text">Ropa casual</p>
+    </div>
+    <div class="card black">
+        <p class="tip">Vestir</p>
+        <p class="second-text">Ropa de vestir</p>
+   
+    </div>
+    </div>
+
+
+</div>
+
+
+
+
+
+
+    </>
+ 
+ )
+}
+
+export default Card
